f2bb2946c6c1dc66c171245f4938a2fd770eeed8
Rohit Singh <27754873+razein97@users.noreply.github.com>
2022-11-13 22:24:27 +0530

    Canonical URL added

--- components/meta.jsx
+++ components/meta.jsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import { useRouter } from "next/router";
+
 const Meta = ({
   title,
   keywords,
@@ -11,10 +13,7 @@ const Meta = ({
 }) => {
   return (
     <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-      ></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords}></meta>
       <meta name="description" content={description}></meta>
       <meta property="og:title" content={ogTitle} />
@@ -22,7 +21,8 @@ const Meta = ({
       <meta property="og:url" content={ogUrl} />
       <meta property="og:image" content={ogImage} />
       <meta charSet="utf-8"></meta>
-      <link rel="icon" href="/favicon.ico"></link>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="canonical" href={canonicalURL()} />
       <title>{title}</title>
     </Head>
   );
@@ -34,3 +34,11 @@ Meta.defaultProps = {
   ogTitle: "Healthootopia",
 };
 export default Meta;
+
+function canonicalURL() {
+  const router = useRouter();
+  const site = "https://healthootopia.com";
+  const canonicalURL = (site + router.asPath).split("?")[0];
+
+  return canonicalURL;
+}
